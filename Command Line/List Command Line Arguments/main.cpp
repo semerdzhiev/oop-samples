6c4a0a4ce0201bc7fe45c03e695d92c11691e7df
Atanas Semerdzhiev <semerdjiev@live.com>
2014-05-14 13:26:20 +0300

    Added a new example: Command Line\List Command Line Arguments

---
+++ Command Line/List Command Line Arguments/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "argc=" << argc << std::endl;
+
+	for(int i = 0; i < argc; i++)
+	{
+		std::cout << "argv["
+				  << i
+				  << "]="
+				  << argv[i]
+				  << std::endl;
+	}
+
+	return 0;
+}
