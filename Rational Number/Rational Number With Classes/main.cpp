b9535eb37aa0b20aa4cf890c338cbb865aa7c0a6
Atanas Semerdzhiev <semerdjiev@live.com>
2014-05-14 14:35:40 +0300

    Added two new examples: 1. Rational Number\Rational Number With Structures 2. Rational Number\Rational Number With Classes

---
+++ Rational Number/Rational Number With Classes/main.cpp
@@ -0,0 +1,238 @@
+﻿#include <iostream>
+
+///
+/// Намира най-големия общ делител на две цели числа
+///
+int gcd(int a, int b)
+{
+	int temp;
+
+	while (b != 0)
+	{
+		temp = a % b;
+
+		a = b;
+		b = temp;
+	}
+	
+	return a;
+}
+
+
+///
+/// Представя рационално число
+///
+class Rational
+{
+public:
+	/// Числителят на рационалното число
+	int Numerator;
+
+	/// Знаменателят на рационалното число
+	int Denominator;
+
+	/// Указва дали рационалното число трябва
+	/// автоматично да се нормализира след всяка операция
+	bool AutoNormalize;
+
+	Rational();
+	Rational(int, int, bool);
+	Rational(bool);
+	~Rational();
+
+	bool IsValid() const;
+	void Add(Rational &);
+
+	void Print() const;
+	void Input();
+
+	void Normalize();
+
+private:
+	inline void PerformAutoNormalize();
+};
+
+
+///
+/// Конструктор по подразбиране
+///
+Rational::Rational()
+{
+	std::cout << "  -> Entering Rational(), object at " << this << std::endl;
+
+	this->Numerator = 0;
+	this->Denominator = 0;
+	this->AutoNormalize = true;
+
+	PerformAutoNormalize();
+}
+
+
+///
+/// Създава ново рационално число с числител Numerator и знаменател Denominator
+///
+Rational::Rational(int Numerator, int Denominator, bool AutoNormalize=true)
+{
+	std::cout << "  -> Entering Rational(" << Numerator << ", " << Denominator << "), object at " << this << std::endl;
+
+	this->Numerator = Numerator;
+	this->Denominator = Denominator;
+	this->AutoNormalize = AutoNormalize;
+
+	PerformAutoNormalize();
+}
+
+
+///
+/// Създава празно рационално число, указвайки дали то да се нормализира автоматично
+///
+Rational::Rational(bool AutoNormalize)
+{
+	std::cout << "  -> Entering Rational(" << (AutoNormalize ? "true" : "false") << "), object at " << this << std::endl;
+
+	this->Numerator = 0;
+	this->Denominator = 0;
+	this->AutoNormalize = AutoNormalize;
+}
+
+///
+/// Деструктор
+///
+Rational::~Rational()
+{
+	std::cout << "  -> Entering ~Rational(), object at " << this << std::endl;
+}
+
+
+///
+/// Връща true, ако данните в обекта са коректни.
+///
+/// Данните се считат за коректни, ако знаменателят е различен от нула.
+///
+bool Rational::IsValid() const
+{
+	return Denominator != 0;
+}
+
+
+///
+/// Добавя стойността на рационалното число other към текущия обект.
+///
+void Rational::Add(Rational& other)
+{
+	Numerator = Numerator*other.Denominator + other.Numerator*Denominator;
+	Denominator = Denominator * other.Denominator;
+
+	PerformAutoNormalize();
+}
+
+
+///
+/// Извежда рационалното число на екрана.
+///
+void Rational::Print() const
+{
+	std::cout << Numerator << "/" << Denominator;
+}
+
+
+///
+/// Въвежда данните за рационалното число от STDIN.
+///
+void Rational::Input()
+{
+	std::cout << "Enter Numerator: ";
+	std::cin >> Numerator;
+	std::cout << "Enter Denominator: ";
+	std::cin >> Denominator;
+
+	PerformAutoNormalize();
+}
+
+
+///
+/// Нормализира рационалното число
+///
+/// Нормализацията прави две неща:
+///
+/// 1. Коригира знаците на числителя и знаменателя:
+/// 1.1 Ако числото е отрицателно, знакът минус се изнася в числителя
+/// 1.2 Ако и числителят и знаменателят са отрицателни, те стават положителни
+/// 
+/// 2. Съкращава числителя и знаменателя на най-големия им общ делител
+///
+void Rational::Normalize()
+{
+	if( ! IsValid() )
+		return;
+
+	// Коригираме знаците
+	if(Denominator < 0)
+	{
+		Numerator = -Numerator;
+		Denominator = -Denominator;
+	}
+
+	// Съкращаваме числителя и знаменателя на най-големия им общ делител
+
+	// Взимаме НОД по модул, за да не обърка знаците при делението
+	int g = abs(gcd(Denominator, Numerator));
+
+	Numerator /= g;
+	Denominator /= g;
+}
+
+
+///
+/// Нормализира числото, но само ако флагът AutoNormalize е вдигнат
+///
+inline void Rational::PerformAutoNormalize()
+{
+	if(AutoNormalize)
+		Normalize();
+}
+
+
+int main()
+{
+	std::cout << "Entering main()\n";
+	
+	std::cout << "Constructing r1\n";
+	Rational r1;
+
+	std::cout << "Constructing a dynamic object\n";
+	Rational *pSingle = new Rational();
+
+	std::cout << "Constructing a dynamic array of five objects\n";
+	Rational *pArray = new Rational[5];
+
+	std::cout << "Destroy the dynamic object\n";
+	delete pSingle;
+
+	std::cout << "Destroy the dynamic array\n";
+	delete [] pArray;
+
+	std::cout << "Create and sum two rational numbers\n";
+	Rational a(1,2);
+	Rational b(2,4);
+	
+	std::cout << "  -> ";
+	a.Print();
+	std::cout << " + ";
+	b.Print();
+	std::cout << " = ";
+	a.Add(b);
+	a.Print();
+	std::cout << "\n";
+
+	std::cout << "Input a rational number from STDIN\n";
+	Rational temp;
+	temp.Input();
+	std::cout << "You entered ";
+	temp.Print();
+	std::cout << std::endl;
+
+	std::cout << "Leaving main()\n";
+
+	return 0;
+}
---
+++ Rational Number/Rational Number With Structures/main.cpp
@@ -0,0 +1,155 @@
+﻿#include <iostream>
+
+///
+/// Представя рационално число
+///
+struct Rational
+{
+	/// Числителят на рационалното число
+	int Numerator;
+
+	/// Знаменателят на рационалното число
+	int Denominator;
+};
+
+
+///
+/// Инициализира една инстанция на рационално число
+///
+void Init(Rational& r, int Numerator=0, int Denominator=0)
+{
+	r.Numerator = Numerator;
+	r.Denominator = Denominator;	
+}
+
+
+///
+/// Проверява дали дадена инстанация на Rational съдържа коректни данни.
+///
+/// Функцията проверява дали знаменателят на числото е различен от нула.
+///
+bool IsValid(const Rational& r)
+{
+	return r.Denominator != 0;
+}
+
+
+///
+/// Събира две рационални числа и връща резултата
+///
+/// \return
+///		Ново рационално число, която съдържа сумата или
+///		0/0, ако някое от r1 и r2 не е валидно рационално число.
+///
+Rational Sum(const Rational& r1, const Rational& r2)
+{
+	Rational result;
+
+	Init(result);
+	
+	if(IsValid(r1) && IsValid(r2))
+	{
+		result.Numerator = r1.Numerator*r2.Denominator + r2.Numerator*r1.Denominator;
+		result.Denominator = r1.Denominator * r2.Denominator;
+	}
+
+	return result;
+}
+
+
+///
+/// Намира най-големия общ делител на две цели числа
+///
+int gcd(int a, int b)
+{
+	int temp;
+
+	while (b != 0)
+	{
+		temp = a % b;
+
+		a = b;
+		b = temp;
+	}
+	
+	return a;
+}
+
+
+///
+/// Нормализира дадено рационално число
+///
+/// Нормализацията прави две неща:
+///
+/// 1. Коригира знаците на числителя и знаменателя:
+/// 1.1 Ако числото е отрицателно, знакът минус се изнася в числителя
+/// 1.2 Ако и числителят и знаменателят са отрицателни, те стават положителни
+/// 
+/// 2. Съкращава числителя и знаменателя на най-големия им общ делител
+///
+void Normalize(Rational& r)
+{
+	if( ! IsValid(r) )
+		return;
+
+	// Коригираме знаците
+	if(r.Denominator < 0)
+	{
+		r.Numerator = -r.Numerator;
+		r.Denominator = - r.Denominator;
+	}
+
+	// Съкращаваме числителя и знаменателя на най-големия им общ делител
+
+	// Взимаме НОД по модул, за да не обърка знаците при делението
+	int g = abs(gcd(r.Denominator, r.Numerator));
+
+	r.Numerator /= g;
+	r.Denominator /= g;
+}
+
+
+///
+/// Извежда рационално число на екрана
+///
+void Print(Rational r)
+{
+	std::cout << r.Numerator << "/" << r.Denominator;
+}
+
+
+int main()
+{
+	// Създаваме три рационални числа
+	Rational r1, r2, r3;
+
+	// Инициализираме числата с начални стойности
+	Init(r1, 1, 3);
+	Init(r2, 2, 4);
+	Init(r3);
+
+	// Ивеждаме числата на екрана
+	std::cout << "r1 = ";
+	Print(r1);
+	std::cout << "\nr2 = ";
+	Print(r2);
+	std::cout << "\nr3 = ";
+	Print(r3);
+	std::cout << std::endl;
+
+	// Събираме две числа
+	r3 = Sum(r2, r2);
+
+	Print(r2);
+	std::cout << " + ";
+	Print(r2);
+	std::cout << " = ";
+	Print(r3);
+	std::cout << std::endl;
+
+	// Нормализираме резултата
+	Normalize(r3);
+	std::cout << "Normalized r3: ";
+	Print(r3);
+	std::cout << std::endl;
+}
