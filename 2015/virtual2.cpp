839289b781088240dd04e6a3ca4d38edc1775d59
Stelf <larodi@gmail.com>
2015-05-11 13:09:17 +0300

    fix tabs

--- 2015/virtual2.cpp
+++ 2015/virtual2.cpp
@@ -9,146 +9,146 @@
 using namespace std;
 
 class Line {
-	protected:
-		float x, y,
-			  x1, y1;
-	public:
-		Line() {
-			cout << "construct Line" << endl;
-		};
+    protected:
+        float x, y,
+              x1, y1;
+    public:
+        Line() {
+            cout << "construct Line" << endl;
+        };
 
-		Line(float _x, float _y, float _x1, float _y1) :
-			x(_x), y(_y), x1(_x1), y1(_y1) {
-			cout << "construct Line" << endl;
-		};
+        Line(float _x, float _y, float _x1, float _y1) :
+            x(_x), y(_y), x1(_x1), y1(_y1) {
+            cout << "construct Line" << endl;
+        };
 
-		virtual const char *whatami() {
-			return "Line";
-		}
+        virtual const char *whatami() {
+            return "Line";
+        }
 
-		virtual void read(ifstream &ifs) {
-			ifs >> x >> y >> x1 >> y1;
-		}
+        virtual void read(ifstream &ifs) {
+            ifs >> x >> y >> x1 >> y1;
+        }
 
-		virtual void write(ofstream &ofs) {
-			ofs << x << ' ' << y << ' '
-				<< x1 << ' ' << y1 << ' ' << endl;
-		}
+        virtual void write(ofstream &ofs) {
+            ofs << x << ' ' << y << ' '
+                << x1 << ' ' << y1 << ' ' << endl;
+        }
 };
 
 typedef unsigned int uint32;
 
 class ColorLine : public Line {
-	protected:
-		uint32 color;
-		float thck;
-	public:
-		ColorLine() {
-			cout << "construct ColorLine" << endl;
-		};
+    protected:
+        uint32 color;
+        float thck;
+    public:
+        ColorLine() {
+            cout << "construct ColorLine" << endl;
+        };
 
-		ColorLine(float _x, float _y, float _x1, float _y1,
-				uint32 _color, float _thck) :
-				Line(_x, _y, _x1, _y1),
-				color(_color), thck(_thck) {
-			cout << "construct ColorLine" << endl;
-		};
+        ColorLine(float _x, float _y, float _x1, float _y1,
+                uint32 _color, float _thck) :
+                Line(_x, _y, _x1, _y1),
+                color(_color), thck(_thck) {
+            cout << "construct ColorLine" << endl;
+        };
 
-		virtual const char *whatami() {
-			return "ColorLine";
-		}
+        virtual const char *whatami() {
+            return "ColorLine";
+        }
 
-		virtual void read(ifstream& ifs) {
-			Line::read(ifs);
-			ifs >> color >> thck;
-		}
+        virtual void read(ifstream& ifs) {
+            Line::read(ifs);
+            ifs >> color >> thck;
+        }
 
-		virtual void write(ofstream& ofs) {
-			Line::write(ofs);
-			ofs << color << ' ' << thck << endl;
-		}
+        virtual void write(ofstream& ofs) {
+            Line::write(ofs);
+            ofs << color << ' ' << thck << endl;
+        }
 };
 
 void writedata() {
-	Line** lines = new Line*[5];
-	cout << "Writing data... " << endl;
+    Line** lines = new Line*[5];
+    cout << "Writing data... " << endl;
 
-	lines[0] = new Line(0, 0, 10, 10);
-	lines[1] = new ColorLine(10, 10, 10, 0, (1<<16) + (100<<8) + 235, 0.235);
-	lines[2] = new ColorLine(10, 0, 0, 0, (1<<16) + (100<<8) + 235, 0.135);
+    lines[0] = new Line(0, 0, 10, 10);
+    lines[1] = new ColorLine(10, 10, 10, 0, (1<<16) + (100<<8) + 235, 0.235);
+    lines[2] = new ColorLine(10, 0, 0, 0, (1<<16) + (100<<8) + 235, 0.135);
 
 #ifdef __GNUC__
-	ofstream ofs("/tmp/storage.txt");
+    ofstream ofs("/tmp/storage.txt");
 #else
-	ofstream ofs("c:\\temp\\storage.txt");
+    ofstream ofs("c:\\temp\\storage.txt");
 #endif
 
-	ofs << 3 << endl;
+    ofs << 3 << endl;
 
-	for (int i = 0; i < 3; i++) {
-		ofs << lines[i]->whatami() << endl;
-		lines[i]->write(ofs);
-	}
+    for (int i = 0; i < 3; i++) {
+        ofs << lines[i]->whatami() << endl;
+        lines[i]->write(ofs);
+    }
 }
 
 void readdata() {
-	Line** lines;
-	cout << "Reading data... " << endl;
+    Line** lines;
+    cout << "Reading data... " << endl;
 #ifdef __GNUC__
-	ifstream ifs("/tmp/storage.txt");
+    ifstream ifs("/tmp/storage.txt");
 #else
-	ifstream ifs("c:\\temp\\storage.txt");
+    ifstream ifs("c:\\temp\\storage.txt");
 #endif
-	int lcount;
+    int lcount;
 
-	ifs >> lcount;
+    ifs >> lcount;
 
-	lines = new Line*[lcount];
+    lines = new Line*[lcount];
 
-	char buf[32];
+    char buf[32];
 
-	for (int i = 0; i < lcount; i++)
-	{
-		//		ifs.getline(buf, 32);
-		ifs >> buf;
-		if (! strcmp(buf, "Line")	) {
-			lines[i] = new Line();
-		}
+    for (int i = 0; i < lcount; i++)
+    {
+        //        ifs.getline(buf, 32);
+        ifs >> buf;
+        if (! strcmp(buf, "Line")    ) {
+            lines[i] = new Line();
+        }
 
-		if (! strcmp(buf, "ColorLine")) {
-			lines[i] = new ColorLine();
-		}
+        if (! strcmp(buf, "ColorLine")) {
+            lines[i] = new ColorLine();
+        }
 
-		lines[i]->read(ifs);
-	}
+        lines[i]->read(ifs);
+    }
 
 }
 
 // for the sake of demostration below
 
 class Wine {
-	public:
-		virtual const char *whatami() {
-			return "Wine";
-		}
+    public:
+        virtual const char *whatami() {
+            return "Wine";
+        }
 };
 
 //
 
 int main()
 {
-	writedata();
-	readdata();
-	return 0;
+    writedata();
+    readdata();
+    return 0;
 
-	// paradox !
-	// object knows its class even though
-	// we cast it to completely different
-	// inheritance tree
+    // paradox !
+    // object knows its class even though
+    // we cast it to completely different
+    // inheritance tree
 
-	Line* a = new Line(1,2,4,6);
-	Wine* b = (Wine*)a;
-	cout << b->whatami();
+    Line* a = new Line(1,2,4,6);
+    Wine* b = (Wine*)a;
+    cout << b->whatami();
 
 }
 
