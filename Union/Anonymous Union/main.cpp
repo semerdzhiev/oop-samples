ea29c18240a961fe3c6cb29a75f64b7c2c5d4040
Atanas Semerdzhiev <semerdjiev@live.com>
2015-05-12 13:16:16 +0300

    New example: Union\Anonymous Union

---
+++ Union/Anonymous Union/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+struct Test
+{
+	unsigned int n1;
+
+	union {
+		unsigned int n2;
+		unsigned short s;
+	};
+};
+
+int main()
+{
+	/*
+	union
+	{
+		unsigned int n;
+		unsigned short s;
+	};
+
+	n = 0x12345678;
+
+	std::cout
+		<< std::hex
+		<< "n = 0x"   << n << " (@ 0x" << &n << ")"
+		<< "\ns = 0x" << s << " (@ 0x" << &s << ")"
+		<< std::endl;
+	*/
+	Test t;
+
+	t.n1 = 0x12345678;
+	t.n2 = 0xAABBCCDD;
+
+	std::cout
+		<< std::hex
+		<< "\nt.n1 = 0x" << t.n1 << " (@ 0x" << &t.n1 << ")"
+		<< "\nt.n2 = 0x" << t.n2 << " (@ 0x" << &t.n2 << ")"
+		<< "\n t.s = 0x" << t.s  << " (@ 0x" << &t.s << ")"
+		<< std::endl;
+
+	return 0;
+}
