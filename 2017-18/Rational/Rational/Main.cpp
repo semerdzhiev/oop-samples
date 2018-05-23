2ef491300887071fc2bbee8a7922f7c2da816180
Atanas Semerdzhiev <semerdjiev@live.com>
2018-05-23 13:19:38 +0300

    *

---
+++ 2017-18/Rational/Rational/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Rational.h"
+
+int main()
+{
+	return 0;
+}
--- 2017-18/Rational/Rational/Rational.cpp
+++ 2017-18/Rational/Rational/Rational.cpp
@@ -1,5 +1,6 @@
-#include "Rational.h"
+#include <stdexcept>
 
+#include "Rational.h"
 
 
 Rational::Rational()
@@ -9,4 +10,27 @@ Rational::Rational()
 
 Rational::~Rational()
 {
+}
+
+int Rational::GetNumerator() const
+{
+	return Numerator;
+}
+
+int Rational::GetDenominator() const
+{
+	return Denominator;
+}
+
+void Rational::SetNumerator(int Value)
+{
+	Numerator = Value;
+}
+
+void Rational::SetDenominator(int Value)
+{
+	if (Value == 0)
+		throw std::invalid_argument("Denominator cannot be zero");
+
+	Denominator = Value;
 }
